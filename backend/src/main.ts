e2cd6904f7f57a54d467d53bcb677392e8e5022e
Nicolas Van Damme <nicolas.van.damme2@hotmail.com>
2026-03-29 12:19:22 +0200

    Try once more, with some logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  const configService = app.get(ConfigService);
+
+  const logger = new Logger();
+  app.useLogger(logger);
+
+  logger.log(
+    [
+      configService.getOrThrow('FRONTEND_URL'),
+      configService.getOrThrow('URL'),
+    ].filter(Boolean),
+  );
+
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, process.env.URL].filter(Boolean),
+    origin: [
+      configService.getOrThrow('FRONTEND_URL'),
+      configService.getOrThrow('URL'),
+    ].filter(Boolean),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Accept,Authorization',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
